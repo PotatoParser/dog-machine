7c4b162ff97e2bc56062bb5c87719618c355fab6
steffitan23 <steffitlw@berkeley.edu>
2022-09-26 02:02:51 -0700

    Add styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,47 +20,31 @@ function App() {
   const prevDog = async () => {
     if (dogIndex <= 0) return;
     setDogIndex(dogIndex - 1);
-    // await updateImage();
   };
 
   const nextDog = async () => {
     if (dogIndex === dogCount) {
-      // Generate next dog
-      // TODO: axios.get('/dog/random')
       await fetch('http://localhost:3001/dog/random')
     }
     setDogIndex(dogIndex + 1);
     setDogCount(dogCount + 1);
-    // await updateImage();
   };
 
   return (
-    <div className="app">
-      <Tile dogIndex={dogIndex}></Tile>
-      <div className="button-group">
-        <Button onClick={prevDog}>
-          Prev
-        </Button>
-        <Button onClick={nextDog}>
-          Next
-        </Button>
+    <div className="app-wrapper">
+      <div className="app">
+        <Tile dogIndex={dogIndex}></Tile>
+        <h1>DOG MACHINE 3000</h1>
+        <div className="button-group">
+          <Button onClick={prevDog}>
+            Previous
+          </Button>
+          <Button onClick={nextDog}>
+            Next
+          </Button>
+        </div>
+        <p>Dog Number: {dogIndex}</p>
       </div>
-      {dogIndex}
-      
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
