1a9ef7f8851c437e7e0cf15df386c3b927024d00
steffitan23 <steffitlw@berkeley.edu>
2022-09-26 16:48:39 -0700

    Remove unused imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Button from './component/button/Button';
-import Tile from './component/tile/Tile';
 
 function App() {
   const [dogIndex, setDogIndex] = useState(0);
